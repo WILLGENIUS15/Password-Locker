6f113a2f06ebfac2ec011b3b01f61860057fe129
fidel omondi <willgenius@gmail.com>
2019-09-01 17:42:47 +0300

    initial commit

--- credentials_test.py
+++ credentials_test.py
@@ -5,7 +5,7 @@ from credentials import Credentials
 class TestCredentials(unittest.TestCase):
     """Test class that defines test cases for the Credentials class behavior
     """
-
+    
     def setUp(self):
         """Set up method to run befor before each test case"""
         self.new_credentials = Credentials("Facebook", "12345")
